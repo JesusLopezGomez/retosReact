60b12835d8805c7e4ad0b9fe0a1ee542f6907120
JesuLopezGomez <jelopez.gom@gmail.com>
2024-01-11 12:33:40 +0100

    .

--- src/reto3/reto3.jsx
+++ src/reto3/reto3.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import "./reto3.css";
+
 export function Reto3(){
     let [wordChange,setWordChange] = useState("");
-    let [liWords,setLiWords] = useState([]); 
+    let [words,setWords] = useState([]); 
 
     const removeWord = (e) => {
         let modList = [];
-        liWords.forEach(li => {
-            console.log(li.props.children[0]);
-            if(li.textContent !== e.target.parentNode.textContent){
-                modList.push(li);
+        words.forEach(word => {
+            if(word !== e.target.parentNode.firstChild.textContent){
+                modList.push(word);
             }
         })
-        setLiWords(modList);
+        setWords(modList);
     }
 
     const changeWord = (e) => {
@@ -20,16 +20,23 @@ export function Reto3(){
     }
     
     const addWord = () => {
-        setLiWords([...liWords, <li key={liWords.length} id={liWords.length}>{wordChange}<strong className="x" onClick={removeWord}> X </strong></li>]);
+        setWords([...words, wordChange]);
     }
     
+    const List = () => {
+        return (
+                <ul>
+                    {words.map(word => 
+                        (<li key={word}>{word}<button className="x" onClick={removeWord}>X</button></li>)
+                    )}
+                </ul>
+        )
+    }
     return(
         <>
             <input type="text" id="word" onChange={changeWord}/>
             <button id="add" onClick={addWord}>Add</button>
-            <ul>
-                {liWords}
-            </ul>
+            <List key={1}></List>
         </>
     )
 }
